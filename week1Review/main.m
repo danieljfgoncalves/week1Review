40a9a0f285c5643bd810a4b259a7d25829e4cfc7
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-10 15:23:23 -0400

    Initial Commit

---
+++ week1Review/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  week1Review
+//
+//  Created by Daniel Goncalves on 2015-04-10.
+//  Copyright (c) 2015 DG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
